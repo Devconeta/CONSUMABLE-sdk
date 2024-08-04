17d6f042f3aa02d9d926403a1c6413b992f30868
Esteban Viera <esteban.viera.arroba@gmail.com>
2024-08-03 21:28:36 -0300

    Create main Consumable class
    Co-authored-by: Alexis Wolfsdorf <alexiswolfsdorf@gmail.com>

--- src/Consumable.ts
+++ src/Consumable.ts
@@ -1,10 +1,23 @@
-class Consumable {
-  private readonly privateKey: string;
+import { ConsumableArguments } from "./types";
 
-  constructor(privateKey: string) {
-    this.privateKey = privateKey;
+export class Consumable {
+  private readonly base64Secret: string;
+
+  constructor(base64Secret: string) {
+    this.base64Secret = base64Secret;
   }
 
-  public async test(): Promise<any> {
+  public consume(): ConsumableArguments {
+    const decodedSecret = Buffer.from(this.base64Secret, 'base64').toString('utf-8');
+    const { privateKey, contractAddress, merkleProof, methodName, methodArgs, chainId } = JSON.parse(decodedSecret) as ConsumableArguments;
+
+    return {
+      contractAddress,
+      chainId,
+      methodName,
+      methodArgs,
+      merkleProof,
+      privateKey,
+    }
   }
 }
